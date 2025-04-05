f48768a5352e22c0727045e69a68f3de3046f89f
Hyoseop Song <crad_on25@naver.com>
2025-04-06 07:48:04 +0900

    🚀 deploy in base hosting url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "loveysuby.github.io",
+    baseUrl: "loveysuby.github.io", 
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
